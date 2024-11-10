87ea4eb776573524861beb8ed7df4fe35eb09627
zoyaaaih <alethafachira@gmail.com>
2024-11-10 17:45:45 +0700

    Add files via upload

---
+++ Cara bikin fungsi dari luas tabung.py
@@ -0,0 +1,13 @@
+import math
+def perkalian (jari, tinggi):
+    hasil = math.pi * jari**2 * tinggi
+    return hasil 
+
+jari = float(input("Masukkan nilai jari-jari: "))
+tinggi = float(input("Masukkan nilai tinggi: "))
+volume= perkalian (jari, tinggi)
+print("Volume tabung adalah: ", volume)
+
+#Aletha Zoya Fachira
+#RPL 1A
+#2405281
